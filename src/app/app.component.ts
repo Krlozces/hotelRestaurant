645083077381f80af0b4796cfc665dbc969ef6c7
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-07-02 00:37:18 -0500

    SearchBar component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SearchBarComponent],
   template: `
     <app-header></app-header>
+    <app-search-bar></app-search-bar>
     <app-footer></app-footer>
   `,
   // templateUrl: './app.component.html',
