41d49e065c1cc37c7e4e8a5f7f9be49333414ddf
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-07-04 18:45:04 -0500

    New path to register a room created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { RegisterRoomComponent } from './register-room/register-room.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'register-room',
+        component: RegisterRoomComponent,
+        title: 'Register room',
+    }
+];
