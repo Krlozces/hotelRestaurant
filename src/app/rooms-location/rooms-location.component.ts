2ff8df3f2981d0da78dac73997fea5aaaf2a4cef
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-07-04 18:48:23 -0500

    Attributes added to the component

--- src/app/rooms-location/rooms-location.component.ts
+++ src/app/rooms-location/rooms-location.component.ts
@@ -1,19 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-rooms-location',
   standalone: true,
   imports: [],
   template: `
-    <section class="listing">
-      <img class="listing-photo" [src]="" alt="" />
-      <h2 class="listing-heading"></h2>
-      <p class="listing-location"></p>
-      <a>Ver más</a>
-    </section>
+    <article class="listing">
+      <!-- <img class="listing-photo" [src]="" alt="" /> -->
+      <h2 class="listing-heading">{{ name }}</h2>
+      <p class="listing-location"><span>Descripción: </span>{{ description }}</p>
+      <p class="listing-location">{{ isBooked }}</p>
+      <p class="listing-location"><span>Capacidad: </span>{{ capacidad }}</p>
+      <p class="listing-location"><span>Tarifa: </span>{{ tarifa }}</p>
+      <a href="#">Ver más</a>
+    </article>
   `,
   styleUrl: './rooms-location.component.scss'
 })
 export class RoomsLocationComponent {
-
+  @Input() name: string;
+  @Input() description: string;
+  @Input() isBooked: boolean;
+  @Input() capacidad: number;
+  @Input() tarifa: number;
 }
