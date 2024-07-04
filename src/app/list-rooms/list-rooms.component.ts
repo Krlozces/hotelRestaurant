0bf29e4d08065a15dd690a034e40a10e97b39a16
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-07-04 14:42:22 -0500

    Component created to list all rooms

---
+++ src/app/list-rooms/list-rooms.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-rooms',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      list-rooms works!
+    </p>
+  `,
+  styleUrl: './list-rooms.component.scss'
+})
+export class ListRoomsComponent {
+
+}
