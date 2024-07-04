64bb4be6ab41943675a4888574c22f2f216b3719
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-07-04 18:42:41 -0500

    List' rooms component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { ListRoomsComponent } from './list-rooms/list-rooms.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, SearchBarComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SearchBarComponent, ListRoomsComponent],
   template: `
     <app-header></app-header>
     <app-search-bar></app-search-bar>
+    <app-list-rooms></app-list-rooms>
     <app-footer></app-footer>
   `,
   // templateUrl: './app.component.html',
