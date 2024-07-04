259cc8b142f8ac054fbfac477dc5bb009967d65d
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-07-04 18:46:05 -0500

    Method created to list all rooms

--- src/app/list-rooms/list-rooms.component.ts
+++ src/app/list-rooms/list-rooms.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { NgFor } from '@angular/common'; 
+import { RoomsLocationComponent } from '../rooms-location/rooms-location.component';
+import { Room } from './../room'
+import { RoomService } from '../room.service';
 @Component({
   selector: 'app-list-rooms',
   standalone: true,
-  imports: [],
+  imports: [RoomsLocationComponent, NgFor],
   template: `
-    <p>
-      list-rooms works!
-    </p>
+    <article *ngFor="let room of rooms">
+      <app-rooms-location 
+        [id]="room.id" 
+        [name]="room.name" 
+        [description]="room.description" 
+        [isBooked]="room.isBooked"
+        [capacidad]="room.capacidad" 
+        [tarifa]="room.tarifa">
+      </app-rooms-location>
+    </article>
   `,
   styleUrl: './list-rooms.component.scss'
 })
-export class ListRoomsComponent {
+export class ListRoomsComponent implements OnInit {
+  rooms: Room[];
+  constructor(private roomService: RoomService) { }
 
+  ngOnInit(): void{
+    this.getRooms();
+  }
+
+  private getRooms(){
+    this.roomService.getListOfRooms().subscribe(data => {
+      this.rooms = data;
+    });
+  }
+  
 }
