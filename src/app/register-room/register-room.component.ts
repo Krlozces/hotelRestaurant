7e03db0e538caa7d44c99cdf3ef2c780cccb8426
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-07-05 22:34:07 -0500

    Form created

--- src/app/register-room/register-room.component.ts
+++ src/app/register-room/register-room.component.ts
@@ -1,33 +1,38 @@
 import { Component } from '@angular/core';
 import { Room } from './../room'; 
+import { RoomService } from '../room.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-register-room',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   template: `
     <div>
       <h3></h3>
-      <form>
+      <form (ngSubmit) = "onSubmit()">
         <div>
-          <label for="name"></label>
+          <label for="name">Nombre: </label>
           <input type="text" name="name" id="name" [(ngModel)]="room.name" />
         </div>
         <div>
-          <label for="description"></label>
+          <label for="description">Descripción: </label>
           <input type="text" name="description" id="description" [(ngModel)]="room.description" />
         </div>
         <div>
-          <label for="name"></label>
-          <input type="checkbox" name="name" id="name" [(ngModel)]="room.name" />
+          <label for="isBooked"></label>
+          <input type="checkbox" name="isBooked" id="isBooked" [(ngModel)]="room.isBooked" />
         </div>
         <div>
-          <label for="name"></label>
-          <input type="checkbox" name="name" id="name" [(ngModel)]="room.name" />
+          <label for="capacidad">Capacidad: </label>
+          <input type="number" name="capacidad" id="capacidad" [(ngModel)]="room.capacidad" />
         </div>
         <div>
-          <label for="name"></label>
-          <input type="checkbox" name="name" id="name" [(ngModel)]="room.name" />
+          <label for="tarifa">Tarifa: </label>
+          <input type="number" name="tarifa" id="tarifa" [(ngModel)]="room.tarifa" />
         </div>
+        <button type="submit">Registrar</button>
       </form>
     </div>
   `,
@@ -35,4 +40,21 @@ import { Room } from './../room';
 })
 export class RegisterRoomComponent {
   room : Room = new Room();
+
+  constructor (private roomService: RoomService, private router: Router) { }
+
+  registerRoom(){
+    this.roomService.registerRoom(this.room).subscribe(data => {
+      console.log(data);
+      this.goToRooms();
+    }, error => console.log(error))
+  }
+
+  goToRooms(){
+    this.router.navigate(['/rooms']);
+  }
+
+  onSubmit(){
+    this.registerRoom();
+  }
 }
