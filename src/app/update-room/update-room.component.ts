2b3a0fbb3d5a2e37f40c2ac725c471385969924a
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-07-08 19:01:58 -0500

    update-room component created

---
+++ src/app/update-room/update-room.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-room',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      update-room works!
+    </p>
+  `,
+  styleUrl: './update-room.component.scss'
+})
+export class UpdateRoomComponent {
+
+}
