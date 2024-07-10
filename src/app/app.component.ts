b38d4c806abd53cf3e9adf1ba377ca786bf58fe6
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-07-10 17:46:34 -0500

    MatDialogModule imported

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { ListRoomsComponent } from './list-rooms/list-rooms.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, SearchBarComponent, ListRoomsComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SearchBarComponent, ListRoomsComponent, MatButtonModule, MatDialogModule],
   template: `
     <app-header></app-header>
     <app-search-bar></app-search-bar>
@@ -20,4 +22,5 @@ import { ListRoomsComponent } from './list-rooms/list-rooms.component';
 })
 export class AppComponent {
   title = 'hotel-restaurant';
+
 }
