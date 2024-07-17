94b38274daf872ad778223fe3fe4a97b56dbe133
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-07-17 11:59:53 -0500

    Content added to the register modal

--- src/app/user-add-edit/user-add-edit.component.ts
+++ src/app/user-add-edit/user-add-edit.component.ts
@@ -6,9 +6,13 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
     <div mat-dialog-title>
-      <h2>User form</h2>
+      <h2>Iniciar sesión o registrarse</h2>
     </div>
-    <div mat-dialog-content></div>
+    <div mat-dialog-content>
+      <h3>Te damos la bienvenida a Hotel&Restaurant</h3>
+      <span>Te vamos a confirmar el número por teléfono o mensaje de texto. Sujeto a las tarifas estándar para mensajes y datos. <a href="#">Política de privacidad</a></span>
+      <button>Continúa</button>
+    </div>
     <div mat-dialog-actions>
       <button mat-button mat-dialog-close>Cancel</button>
       <button mat-button mat-dialog-close>Save</button>
