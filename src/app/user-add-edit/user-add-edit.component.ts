490a36224c2930d2c413fb6e865893c55596cef0
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-07-21 14:00:31 -0500

    Btns commented

--- src/app/user-add-edit/user-add-edit.component.ts
+++ src/app/user-add-edit/user-add-edit.component.ts
@@ -11,10 +11,14 @@ import { NgFor } from '@angular/common';
   imports: [FontAwesomeModule, NgFor],
   template: `
     <div mat-dialog-title>
+      <div class="close-icon">
+        <img src="../../assets/images/close_round_icon.svg" alt="close-icon" width="16px" height="16px" />
+      </div>
+      <hr/>
       <h2>Iniciar sesión o registrarse</h2>
     </div>
     <hr/>
-    <div mat-dialog-content>
+    <div mat-dialog-content class="modal-content-scroll">
       <h3>Te damos la bienvenida a Hotel&Restaurant</h3>
       <div class="inputs-container">
         <select name="code-countries" id="code-countries" class="phone-number">
@@ -29,27 +33,27 @@ import { NgFor } from '@angular/common';
       <hr/>
       <div class="btns-container">
         <a href="#" class="btn-brand">
-          <fa-icon [icon] = "faSquareFacebook" size="lg"></fa-icon>
+          <fa-icon [icon]="faSquareFacebook" size="lg"></fa-icon>
           <span>Continúa con Facebook</span>
         </a>
         <a href="#" class="btn-brand">
-          <fa-icon [icon] = "faGoogle" size="lg"></fa-icon>
+          <fa-icon [icon]="faGoogle" size="lg"></fa-icon>
           <span>Continúa con Google</span>
         </a>
         <a href="#" class="btn-brand">
-          <fa-icon [icon] = "faApple" size="lg"></fa-icon>
+          <fa-icon [icon]="faApple" size="lg"></fa-icon>
           <span>Continúa con Apple</span>
         </a>
         <a href="#" class="btn-brand">
-          <fa-icon [icon] = "faEnvelope" size="lg"></fa-icon>
+          <fa-icon [icon]="faEnvelope" size="lg"></fa-icon>
           <span>Continúa con el correo electrónico</span>
         </a>
       </div>
     </div>
-    <div mat-dialog-actions>
+    <!-- <div mat-dialog-actions>
       <button mat-button mat-dialog-close>Cancel</button>
       <button mat-button mat-dialog-close>Save</button>
-    </div>
+    </div> -->
   `,
   styleUrl: './user-add-edit.component.scss'
 })
